b54de130508968abffc5c64798930894b09db5c1
Elena Semina <stanovaya.elena@gmail.com>
2022-02-11 10:28:05 +0200

    add dashboard

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,10 +64,20 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+        $user->assignRole('user');
+
+        return $user;
+        //old code with not role
+        /*return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);*/
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ResultController;
 use App\Http\Controllers\CheckController;
-
+use App\Http\Controllers\Admin\HomeController;
+//use \Spatie\Permission\Models\Role;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -28,5 +29,15 @@ Route::get('/', function () {
 
 Route::get('/check',[CheckController::class, 'index'])->name('pages.check');
 Route::post('/check',[CheckController::class, 'checkDNSname'])->name('submit-form');
-
 Route::get('/result', [ResultController::class, 'index'])->name('pages.result');
+
+
+
+Route::middleware(['role:admin'])->prefix('administrator')->group(function () {
+
+    Route::get('/', [HomeController::class, 'index'])->name('administrator'); // /administrator
+    Route::get('/settings', [HomeController::class, 'settings']); // /administrator
+//    Route::resource('category', CategoryController::class);
+//    Route::resource('post', PostController::class);
+});
+
